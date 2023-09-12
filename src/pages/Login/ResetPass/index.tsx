f435de3e61248f1ee9e370a83cf0ed2d9bedb396
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-09-11 23:20:45 -0400

    Rerf

--- src/pages/Login/ResetPass/index.tsx
+++ src/pages/Login/ResetPass/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { LoginButton, LoginForm, Resend, WelcomeText } from "./style";
