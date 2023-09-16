c6b8c54de1c69ef1719ada3143de18d3e4eca70d
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-09-15 21:28:18 -0400

    Rerf

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import { SignUp } from "../pages/Login/Signup";
-import { SignInPsy } from "../pages/Login/Psychologist/Signin";
-import { SignIn } from "../pages/Login/Patient/Signin";
+
+import { StartLogin } from "../pages/Login/Start";
 import { HomePsy } from "../pages/Home/Psychologist";
 import { RequireAuth } from "../contexts/auth/RequireAuth";
-import { StartLogin } from "../pages/Login/Start";
+import { SignInPsy } from "../pages/Login/Psychologist/Signin";
 import { RecoverPass } from "../pages/Login/Recover/RecoverPass";
+import { SignIn } from "../pages/Login/Patient/Signin";
+import { SignUp } from "../pages/Login/Patient/Signup";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<RequireAuth><HomePsy /></RequireAuth>} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <HomePsy />
+          </RequireAuth>
+        }
+      />
       <Route path="/start-login" element={<StartLogin />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
