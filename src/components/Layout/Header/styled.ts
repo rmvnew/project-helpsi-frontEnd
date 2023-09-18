58cff7533b3e5a5803e4c68d98f2d9ab6b04b4c0
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-09-18 11:13:19 -0400

    fix

--- src/components/Layout/Header/styled.ts
+++ src/components/Layout/Header/styled.ts
@@ -4,7 +4,7 @@ export const Header = styled.header`
   width: 100%;
   padding: 20px 0;
   background-color: var(--bg-secundary);
-  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: stretch;
 
