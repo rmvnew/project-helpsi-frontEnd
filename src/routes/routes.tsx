9cc990707e4c1f1b5145da7068b9947914bdd754
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-09-19 12:28:32 -0400

    Rerf : url

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -13,16 +13,16 @@ import { Scheduling } from "../pages/Scheduling";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePsy />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/start-login" element={<StartLogin />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin-psy" element={<SignInPsy />} />
-      <Route path="/signup-psy" element={<SignUpPsy />} />
-      <Route path="/recover-pass" element={<RecoverPass />} />
-      <Route path="/reset-pass" element={<ResetPass />} />
-      <Route path="/scheduling" element={<Scheduling />} /> 
+      <Route path="/home/patient" element={<HomePsy />} />
+      <Route path="/home/psy" element={<Home />} />
+      <Route path="/" element={<StartLogin />} />
+      <Route path="/login/recover-pass" element={<RecoverPass />} />
+      <Route path="/login/reset-pass" element={<ResetPass />} />
+      <Route path="/signin/patient" element={<SignIn />} />
+      <Route path="/signup/patient" element={<SignUp />} />
+      <Route path="/signin/psy" element={<SignInPsy />} />
+      <Route path="/signup/psy" element={<SignUpPsy />} />
+      <Route path="/scheduling" element={<Scheduling />} />
     </Routes>
   );
 };
