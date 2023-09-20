aa08398eee68fa2451a5952ba911cd9cd730e0e7
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-09-19 22:18:21 -0400

    refactor

--- src/pages/Home/Patient/index.tsx
+++ src/pages/Home/Patient/index.tsx
@@ -3,17 +3,11 @@ import {
   Main,
 } from "../../../components/Layout/Container/ContainerHome/styled";
 import { ContainerWrapper } from "../../../components/Layout/Container/style";
-import { HeaderWrapper } from "../../../components/Layout/Header";
-import Sidebar from "../../../components/Layout/Sidebar/Patient";
-
 
 export const Home = () => {
   return (
     <>
       <ContainerWrapper>
-        <Sidebar />
-        <HeaderWrapper />
-
         <Container>
           <Main>
             <h1 style={{ color: "red" }}>Tela de Paciente</h1>
--- src/pages/Home/Psychologist/index.tsx
+++ src/pages/Home/Psychologist/index.tsx
@@ -1,16 +1,14 @@
-import { Container, Main } from "../../../components/Layout/Container/ContainerHome/styled";
+import {
+  Container,
+  Main,
+} from "../../../components/Layout/Container/ContainerHome/styled";
 import { ContainerWrapper } from "../../../components/Layout/Container/style";
-import { HeaderWrapper } from "../../../components/Layout/Header";
-import SidebarPsy from "../../../components/Layout/Sidebar/Psychologist";
-
 
 export const HomePsy = () => {
   return (
     <>
       <ContainerWrapper>
-        <SidebarPsy />
         <Container>
-          <HeaderWrapper />
           <Main>
             <h1 style={{ color: "red" }}>Tela de Psicologo</h1>
           </Main>
