9d40b8a2018a7cb9cab8fd602c16abd6333ba256
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-10-01 00:06:36 -0400

    refactor: AurhProvider

--- src/contexts/auth/AuthProvider.tsx
+++ src/contexts/auth/AuthProvider.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import { useApi } from "../../hooks/useApi";
 import { AuthContext } from "./AuthContext";
 import { toast, ToastContainer } from "react-toastify";
@@ -11,49 +10,13 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const [user, setUser] = useState<Payload | null>(null);
   const [loading, setLoading] = useState(true);
   const api = useApi();
-  const storageData = localStorage.getItem("authToken");
 
-  useEffect(() => {
-    if (storageData) {
-      try {
-        const decodedToken: any = jwtDecode(storageData);
-
-        if (decodedToken.exp * 1000 > Date.now()) {
-          
-          const userData: Payload = {
-            name: decodedToken.name,
-            email: decodedToken.email,
-            
-          };
-          setUser(userData);
-        } else {
-          // Token is expired
-          setUser(null);
-          localStorage.removeItem("authToken");
-          toast.error("Sua sessão expirou. Por favor, faça login novamente.");
-        }
-      } catch (error) {
-        
-        setUser(null);
-        localStorage.removeItem("authToken");
-        toast.error("Erro ao decodificar o token.");
-      }
-    } else {
-      setUser(null);
-    }
-    setLoading(false);
-  }, [storageData]);
-
-  const signin = async (email: string, password: string) => {
-    const data = await api.signin(email, password);
-
-    if (data && data.status === undefined) {
+  const handleApiResponse = (data: any) => {
+    if (data && !data.status) {
       setUser(data);
       setToken(data.access_token);
-
       localStorage.setItem("userLogin", data.login);
       localStorage.setItem("userProfile", data.profile);
-
       return {
         message: "pass",
         code: 200,
@@ -61,50 +24,55 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
       };
     }
 
-    let message =
-      data && data.message ? data.message : "Error: No message provided";
-    let code = data ? data.code : 0;
-
+    const { message = "Error: No message provided", code = 0 } = data || {};
     return {
-      message: message,
-      code: code,
+      message,
+      code,
       status: false,
     };
+  };
+
+  const clearUserLocalStorage = () => {
+    ["authToken", "userLogin", "userProfile"].forEach((item) => {
+      localStorage.removeItem(item);
+    });
+  };
+
+  useEffect(() => {
+    const storageData = localStorage.getItem("authToken");
+    if (storageData) {
+      try {
+        const decodedToken: any = jwtDecode(storageData);
+        if (decodedToken.exp * 1000 > Date.now()) {
+          setUser({
+            name: decodedToken.name,
+            email: decodedToken.email,
+          });
+        } else {
+          clearUserLocalStorage();
+          toast.error("Sua sessão expirou. Por favor, faça login novamente.");
+        }
+      } catch (error) {
+        clearUserLocalStorage();
+        toast.error("Erro ao decodificar o token.");
+      }
+    }
+    setLoading(false);
+  }, []);
+
+  const signin = async (email: string, password: string) => {
+    const data = await api.signin(email, password);
+    return handleApiResponse(data);
   };
 
   const signinWithGoogle = async (idToken: string) => {
     const data = await api.signinWithGoogle(idToken);
-
-    if (data && data.status === undefined) {
-      setUser(data);
-      setToken(data.access_token);
-
-      localStorage.setItem("userLogin", data.login);
-      localStorage.setItem("userProfile", data.profile);
-
-      return {
-        message: "pass",
-        code: 200,
-        status: true,
-      };
-    }
-
-    let message = data && data.message ? data.message : "Error: No message provided";
-    let code = data ? data.code : 0;
-
-    return {
-      message: message,
-      code: code,
-      status: false,
-    };
+    return handleApiResponse(data);
   };
 
   const signout = async () => {
     setUser(null);
-    setToken("");
-
-    localStorage.removeItem("userLogin");
-    localStorage.removeItem("userProfile");
+    clearUserLocalStorage();
     await api.logout();
   };
 
@@ -113,7 +81,9 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, signin, signout, loading, signinWithGoogle }}>
+    <AuthContext.Provider
+      value={{ user, signin, signout, loading, signinWithGoogle }}
+    >
       {children}
       <ToastContainer />
     </AuthContext.Provider>
