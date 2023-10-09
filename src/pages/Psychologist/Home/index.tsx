406b1a6bc3a54952c4befa0239467aa6768bb614
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-10-09 12:33:56 -0400

    feat: implement router and mobile first

--- src/pages/Psychologist/Home/index.tsx
+++ src/pages/Psychologist/Home/index.tsx
@@ -1,6 +1,6 @@
 import {
   Container,
-  Main
+  Main,
 } from "../../../components/Layout/Container/ContainerHome/styled";
 import { Body } from "../../../components/Layout/Container/style";
 import Header from "../../../components/Layout/Header/psy";
@@ -10,46 +10,47 @@ import icon2 from "../../../assets/icons/Health_Checkup.svg";
 import icon3 from "../../../assets/icons/Journal.svg";
 import icon4 from "../../../assets/icons/Google_Docs.svg";
 import icon5 from "../../../assets/icons/Forward _Arrow.svg";
+import { Link } from "react-router-dom";
 
 export const HomePsy = () => {
   return (
     <>
-            <Body>
+      <Body>
         <Header />
         <Container>
           <Main>
             <div className="corpo">
               <div className="container1 center-container1">
-                <div className="card card1">
+                <Link to="/psy/schedule" className="card card1">
                   <img className="icones" src={icon1} alt="google calender" />
-                  <h1 className="h1">Minha Agenda</h1>
-                </div>
 
-                <div className="card card2">
-                 
+                  <span className="link"> Minha Agenda</span>
+                </Link>
+
+                <Link to="/psy/patients" className="card card2">
                   <img className="icones" src={icon2} alt="Helth checkup" />
-                  <h1 className="h1">Meus Pacientes</h1>
-                </div>
+                  <span className="link"> Meus Pacientes</span>
+                </Link>
 
-                <div className="card card3">
-                  
+                <Link to="/daily" className="card card3">
                   <img className="icones" src={icon3} alt="Diário de Emoções" />
-                  <h1 className="h1">Diário de Emoções</h1>
-                </div>
-              </div> {/* container1 */}
+
+                  <span className="link">Diário de Emoções</span>
+                </Link>
+              </div>
 
               <div className="container2">
-                <div className="card card4">
-                  
+                <Link to="/documents" className="card card4">
                   <img className="icones" src={icon4} alt="Documentos" />
-                  <h1 className="h1">Documentos</h1>
-                </div>
 
-                <div className="card card5">
+                  <span className="link">Documentos</span>
+                </Link>
+
+                <Link to="/encaminhamentos" className="card card5">
                   <img className="icones" src={icon5} alt="Encaminhamentos" />
-                  <h1 className="h1">Encaminhamentos</h1>
-                </div>
-              </div> {/* container2 */}
+                  <span className="link">Encaminhamentos</span>
+                </Link>
+              </div>
             </div>
           </Main>
         </Container>
