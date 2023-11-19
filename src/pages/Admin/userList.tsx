52639456e4fdf699ac3a9773a02de65b7a213951
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-11-19 18:34:50 -0400

    fix: hidden admin

--- src/pages/Admin/userList.tsx
+++ src/pages/Admin/userList.tsx
@@ -9,16 +9,16 @@ type UserListProps = {
   onShowForm: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const UserList: React.FC<UserListProps> = ({
-  users,
-  searchValue,
-  onEditClick,
-}) => {
+export const UserList: React.FC<UserListProps> = ({ users, searchValue, onEditClick }) => {
+
   const [currentUsers, setCurrentUsers] = useState<User[]>(users);
 
   const handleUserDeletion = (deletedUserId: string) => {
+
     setCurrentUsers((prevUsers) =>
+
       prevUsers.filter((user) => user.user_id !== deletedUserId)
+
     );
   };
 
@@ -27,21 +27,27 @@ export const UserList: React.FC<UserListProps> = ({
   }, [users]);
 
   const filteredUsers = currentUsers.filter((user) =>
+
     user.user_name.toLowerCase().includes(searchValue.toLowerCase())
+
   );
 
   return (
     <div>
       {filteredUsers.map((user) => (
-        <UserItem
-          key={user.user_name}
-          userName={user.user_name}
-          userId={user.user_id}
-          onDelete={handleUserDeletion}
-          onEdit={() => onEditClick && onEditClick(user)}
-        />
+       
+        user.user_name === "SYSADMIN" ? null : (
+
+          <UserItem
+            key={user.user_name}
+            userName={user.user_name}
+            userId={user.user_id}
+            onDelete={handleUserDeletion}
+            onEdit={() => onEditClick && onEditClick(user)}
+          />
+          
+        )
       ))}
     </div>
   );
 };
-  
